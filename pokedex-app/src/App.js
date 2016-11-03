dba97f3ff83381ee4edd92b3ee4da7472b217708
vmjoseph <valliejoseph@gmail.com>
2016-11-03 02:40:48 -0400

    Got API call to work, outputs to user defined layer

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,53 +1,79 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-
+import ReactDOM from 'react-dom';
 class App extends Component {
 	constructor(props) {
     super(props);
-    this.state = {value: ''}; 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      value: '',
+      data: {} //filled by fetch data from API
+    };
   }
+  
 
-  handleChange(event) {
+   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
-   // console.log('Text field value is: ' + this.state.value);
-   
-   axios.get('https://pokeapi.co/api/v2/pokemon/'+ this.state.value+ '/')
-		.then(function (response) {
-		console.log(response.data); // ex.: { user: 'Your User'}
-		console.log(response.status); // ex.: 200
-		
-		
-		
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
-	
-  }
+    alert('Text field value is: ' + this.state.value);
+     var _this = this;
+		fetch('https://pokeapi.co/api/v2/pokemon/'+this.state.value+'/')  
+		  .then(  
+			function(response) {  
+			  if (response.status !== 200) {  
+				console.log('Looks like there was a problem. Status Code: ' +  
+				  response.status);  
+				return;  
+			  }
+			  // Examine the text in the response  
+			  response.json().then(function(data) {  
+				console.log(data.name +" "+ data.id);
+                _this.setState({data: data});
+
+			  });  
+			} 
+		  )  
+		  .catch(function(err) {  
+			console.log('Fetch Error :-S', err);  
+            _this.setState({data: {}});
+		  });
+
+		  }
+
+
   render() {
+	  
+	  var data = this.state.data;
+	  
     return (
+    
+    
+    
       <div className="App">
+      
+      
         <div className="App-header">
-          <h2>Reactdex</h2>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-          <input type="text"placeholder="type the name of the pokemon"
-          value={this.state.value}
-          onChange={this.handleChange} />
-        <button onClick={this.handleSubmit}>
-          Submit
-        </button>
+        
+        <input type="text"
+        placeholder="enter name of pokemon here"
+        value={this.state.value}
+        onChange={this.handleChange.bind(this)}
+        />
+        <button type="button" onClick={this.handleSubmit.bind(this)}>Search the Pokedex</button>
+      <h3>{data.id}</h3>       
+      <h3>{data.name}</h3>
+        
       </div>
+      
+      
+      
     );
+   
   }
 }
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-
 export default App;
